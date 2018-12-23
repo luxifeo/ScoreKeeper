f9f0c3bdd21f23418cd95d9fedb1457ad5f2716a
luxifeo <tttennguoidung@gmail.com>
2018-12-23 22:48:56 +0700

    Add menu, day/night mode

--- app/src/main/java/com/example/hoa/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/hoa/scorekeeper/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.hoa.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     private int mScore2;
     private TextView mScoreText1;
     private TextView mScoreText2;
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         mScoreText2 = findViewById(R.id.score_2);
         mScore1 = Integer.valueOf(mScoreText1.getText().toString());
         mScore2 = Integer.valueOf(mScoreText2.getText().toString());
+        // For recreating the data
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            //Set the score text views
+            mScoreText1.setText(String.valueOf(mScore1));
+            mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 
     public void decreaseScore(View view) {
@@ -44,5 +59,39 @@ public class MainActivity extends AppCompatActivity {
                 mScore2++;
                 mScoreText2.setText(String.valueOf(mScore2));
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.night_mode) {
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        // Recreate the activity for the theme change to take effect.
+        recreate();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the scores.
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
     }
 }
