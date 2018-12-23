2748e565361b61d8257c3aac9553b9be52b6272e
luxifeo <tttennguoidung@gmail.com>
2018-12-23 16:28:24 +0700

    Firstss Commit

---
+++ app/src/main/java/com/example/hoa/scorekeeper/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.hoa.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int mScore1;
+    private int mScore2;
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+        mScore1 = Integer.valueOf(mScoreText1.getText().toString());
+        mScore2 = Integer.valueOf(mScoreText2.getText().toString());
+    }
+
+    public void decreaseScore(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    public void increaseScore(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+}
